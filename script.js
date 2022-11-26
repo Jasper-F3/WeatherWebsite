7d2a24170f7aebfbf46bd89c05b0de6ccf2d2ab1
Rafael Vargas <rafael.vargas.emp@gmail.com>
2022-11-25 21:37:53 -0300

    inserido valores card principal

--- script.js
+++ script.js
@@ -3,11 +3,46 @@ async function getDados(url) {
   return dados.json();
 }
 
-async function imprimirDados(url) {
-  const d = await getDados(url);
-  console.log(d[0].temperatura);
-}
+async function getLeituras(url) {
+  const dados = await getDados(url);
+  const divTemp = document.querySelector("#temp");
+  const divHumidade = document.querySelector("#humidade");
+  const divVento = document.querySelector("#vento");
+  var hoje;
+  dados.forEach((element) => {
+    const day = dados[dados.length - 1];
 
-document.querySelector("#info").addEventListener("click", async () => {
-  await imprimirDados("http://localhost:3000/dados");
-});
+    if (element === day) {
+      // dados
+      const temp = Math.ceil(day.temperatura);
+      const humidade = day.umidade;
+      const vento = day.vento;
+      // criação do texto para o html
+      const tempAdd = document.createTextNode(temp);
+      const humiAdd = document.createTextNode(humidade);
+      const ventoAdd = document.createTextNode(vento);
+      // insert dos dados no html
+      divTemp.append(tempAdd, "°");
+      divHumidade.append(humiAdd, "%");
+      divVento.append(ventoAdd, "km/h");
+    }
+    for (let data in day) {
+      if (data === "createdAt") {
+        var dataHoje = day[data];
+      }
+    }
+    var arrayHoje = dataHoje.split("T");
+    hoje = arrayHoje[0].split("-");
+    hoje = parseInt(hoje[2]);
+  });
+  for (let dias in dados) {
+    var dataDia = dados[dias].createdAt;
+    var arrayDia = dataDia.split("T");
+    dia = arrayDia[0].split("-");
+    dia = parseInt(dia[2]);
+
+    if (dia === hoje - 2) {
+    }
+  }
+}
+getLeituras("http://localhost:3000/dados");
